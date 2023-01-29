c63ffe98154e7460a64f1bb1dbcbc92968c945a0
Tanya <zavoronkova1981@gmail.com>
2023-01-29 21:58:41 +0300

    added text

--- C#Lesson1/Seminar1/Program.cs
+++ C#Lesson1/Seminar1/Program.cs
@@ -21,6 +21,7 @@ int b = Convert.ToInt32(Console.ReadLine());
   
  
 //Задача 4. Напишите программу,которая принимает на вход три числа и выдает максимальное из этих чисел.  
+/*  
   
 
 int Max (int arg1,int arg2,int arg3)  
@@ -44,4 +45,73 @@ int max1 = Max (a1,b1,c1);
 int max2 = Max (a2,b2,c2);  
 int max3 = Max (a3,b3,c3);  
 int max = Max(max1,max2,max3);  
-Console.WriteLine(max);
+Console.WriteLine(max);//
+//
+  
+// Задача 6 Напиши программу,которая на вход принемает число и выдает,является ли число четным(делится ли оно на два без остатка)  
+   */    
+   /*  
+{  
+    int i;  
+    Console.Write("4:");  
+    i = int.Parse(Console.ReadLine());  
+    if(i % 2 ==0)  
+{  
+    Console.Write("Введенное число является четным");  
+    Console.Read();  
+}  
+ else  
+{  
+    Console.Write("Введенное число нечетное");  
+    Console.Read();  
+}  
+} 
+*/  
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.    
+/*
+int i = 1;  
+bool not = true;  
+Console.WriteLine("Введите число:");  
+num =Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("Четные числа от 1 до" + num);  
+while (i <= num) 
+{  
+    if(i % 2 != 1)  
+    {  
+    Console.Write(i + ",");  
+    not = false;  
+    }  
+    i++;  
+}  
+if(not)  
+{  
+    Console.WriteLine("Нет четных чисел");  
+}  
+ break;  
+ default:  
+ begin = false;  
+ break;  
+ }  
+ }
+  */  
+  //Задача 2 Напишите программу,которая на вход принемает два числа и выдает,какое число большее,а какое меньшее    
+  /*
+  Console.WriteLine("Введите первое число:");  
+  int num_A = Convert.ToInt32(Console.ReadLine());  
+  Console.WriteLine("Введите второе число:");  
+  int num_B = Convert.ToInt32(Console.ReadLine());  
+  if(num_A > num_B)  
+  {  
+    Console.WriteLine("Первое число"+ num_A + "больше чем второе" + num_B);  
+  }  
+  else  
+  {  
+    Console.WriteLine("Второе число"+ num_B + "больше чем первое" + num_A);  
+  }   
+  */ 
+   
+
+
+
+
+
