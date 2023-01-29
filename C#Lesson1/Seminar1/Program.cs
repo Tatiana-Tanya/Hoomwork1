bd525bafc4ac74720643f7deb63004766c82eb86
Tanya <zavoronkova1981@gmail.com>
2023-01-29 19:13:45 +0300

    added text

--- C#Lesson1/Seminar1/Program.cs
+++ C#Lesson1/Seminar1/Program.cs
@@ -29,4 +29,24 @@ int CutNumber(int number)
 }  
 int num = new Random().Next(100,1000);    
 //  
-
+// Задача 4. Напишите программу,которая принимает на вход три числа и выдает максимальное из этих чисел.  
+int Max (int arg1,int arg2,int arg3)  
+{  
+    int result = arg1;  
+    if(arg2 > result)result = arg2;  
+    if(arg3 >result)result = arg3;  
+    return result  
+}  
+int a1 = 2  
+int b1 = 7  
+int c1 = 3  
+int a2 = 78  
+int b2 = 44  
+int c2 = 5  
+int a3 = 9  
+int b3 = 3  
+int c3 = 22  
+int max1 = Max(a1,b1,с1);   
+int max2 = Max(a2,b2,c2);  
+int max3 = Max(a3,b3,c3);  
+Console.WriteLine("max1,max2,max3");
