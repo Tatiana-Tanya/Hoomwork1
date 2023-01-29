5ff5c86266fb849ce86a0e93f33004fa52bb0385
Tanya <zavoronkova1981@gmail.com>
2023-01-29 20:04:50 +0300

    added text

--- C#Lesson1/Seminar1/Program.cs
+++ C#Lesson1/Seminar1/Program.cs
@@ -17,36 +17,31 @@ int b = Convert.ToInt32(Console.ReadLine());
 //  
 // Задача 1 Напишите программу,которая принемает во вход трехзначное читсло и удаляет вторую цифру этого числа  
 //    
-//
-int CutNumber(int number)    
 
-{  
-    int sot = number / 100;  
-    int ed = number % 10;  
-      
-    int result  = sot * 10 + ed;  
-    return resurt ;  
-}  
-int num = new Random().Next(100,1000);    
-//  
-// Задача 4. Напишите программу,которая принимает на вход три числа и выдает максимальное из этих чисел.  
+  
+ 
+//Задача 4. Напишите программу,которая принимает на вход три числа и выдает максимальное из этих чисел.  
+  
+
 int Max (int arg1,int arg2,int arg3)  
 {  
     int result = arg1;  
-    if(arg2 > result)result = arg2;  
-    if(arg3 >result)result = arg3;  
-    return result  
-}  
-int a1 = 2  
-int b1 = 7  
-int c1 = 3  
-int a2 = 78  
-int b2 = 44  
-int c2 = 5  
-int a3 = 9  
-int b3 = 3  
-int c3 = 22  
-int max1 = Max(a1,b1,с1);   
-int max2 = Max(a2,b2,c2);  
-int max3 = Max(a3,b3,c3);  
-Console.WriteLine("max1,max2,max3");
+    if(arg2 > result)result = arg2; 
+    if(arg3 > result)result = arg3;  
+    return result;    
+}
+
+int a1 = 2 ;  
+int b1 = 7 ; 
+int c1 = 3 ; 
+int a2 = 7 ;
+int b2 = 44 ;
+int c2 = 5 ;
+int a3 = 9 ;
+int b3 = 3 ;
+int c3 = 22 ;
+int max1 = Max (a1,b1,c1);   
+int max2 = Max (a2,b2,c2);  
+int max3 = Max (a3,b3,c3);  
+int max = Max(max1,max2,max3);  
+Console.WriteLine(max);
