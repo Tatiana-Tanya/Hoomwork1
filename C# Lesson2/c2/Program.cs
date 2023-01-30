dc642a47ed1a7c1d1e449b961c98b466457ef927
Tanya <zavoronkova1981@gmail.com>
2023-01-30 22:04:57 +0300

    added text

---
+++ C# Lesson2/c2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+int CutNumber(int number)  
+
+{
+            int result = -1;
+            if (number >= 100)
+    {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }  
+*/  
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа.  
+int MakeArray(int a, int b)
+{
+int result = 0;
+ if (b < 3)
+    {
+ Console.Write("Третьей цифры нет,только ");
+    }
+ else
+    {
+ int c = 1;
+ for (int i = b; i > 3; i--)
+        {
+ c = c * 10;
+        }
+
+ result = (a / c) % 10;
+    }
+return result;
+}
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
