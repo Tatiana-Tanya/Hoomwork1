1fcba5cd6ca6c7c69591775e6c28191ece133d5c
Tanya <zavoronkova1981@gmail.com>
2023-02-01 21:58:04 +0300

    added text

--- C# Lesson2/c2/Program.cs
+++ C# Lesson2/c2/Program.cs
@@ -99,5 +99,31 @@ else
 {
     Console.WriteLine(InCenter(number));
 }  
-*/
+    
+
+  //Console.Write("Input an integer number.");  
+  //int num = Convert.ToInt32(Console.ReadLine());     
+  ---------------------- 
+  //Задача 1.Напишите программу,которая на вход принемает два числа и проверяет,является ли первое число квадратом второго.
+
+    //Console.Write("input a first number:");  
+ int num1 = Convert.ToInt32(Console.ReadLine());  
+  Console.Write("Input a second number:");  
+  int num2 = Convert.ToInt32(Console.ReadLine());  
+  int quad2 = num2 * num2;  
+  if (num1 == quad2)  
+  {  
+      Console.WriteLine("Yes");  
+  }   
+  else  
+  {  
+      Console.WriteLine("No");  
+  }    
+   
+ //Задача 2. Напишите программу,которая на вход принемает одно число(N),а на выходе показывает все целые числа в промежутке от -N до N.  
+   
+
+    
+  
+
 
