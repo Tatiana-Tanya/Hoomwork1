7cacb5544dca2fa944d0d560d8052f41e0b5deeb
Tanya <zavoronkova1981@gmail.com>
2023-02-05 22:03:32 +0300

    added text

--- C#Lesson3/HoomWork/Program.cs
+++ C#Lesson3/HoomWork/Program.cs
@@ -87,6 +87,51 @@ else
 {
  Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
 }  
-*/   
+*/    
+// Задача 19.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.  
+/* 
+
+void CheckPalindromicNumber(int number)
+{
+    if(number >= 10000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+ 
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int division2 = number / 100;
+                int remainder2 = number % 10;
+                if(division2 == remainder2)
+                    Console.WriteLine("Да");
+            }
+            else 
+            Console.WriteLine("Нет");
+            
+    }
+    else
+    Console.WriteLine("Некорректное число!");
+}
+ 
+Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine()!);
+CheckPalindromicNumber(number);  
+*/    
+// Задача 19.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.   
+/*
+ Console.Write("Enter a number to check if palindrome: ");
+        bool palindrome = true;
+        int x = int.Parse(Console.ReadLine());int c = x.ToString().Length - 1;
+        string b = x.ToString();
+        for (int i = 0; i < c; i++)
+            if (b[i] != b[c - i])
+                palindrome = false;
+        if (palindrome == true)
+            Console.Write("Yes");
+        else Console.Write("No");
+        Console.ReadKey();  
+  */  
+        
 
 
