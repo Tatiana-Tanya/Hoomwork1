42522d524c4b6d4dfd5f7c25e812e1d3012b8fce
Tanya <zavoronkova1981@gmail.com>
2023-02-10 05:35:34 +0300

    added text

--- C#Lesson4/HoomWork/Program.cs
+++ C#Lesson4/HoomWork/Program.cs
@@ -94,6 +94,34 @@ int ReadInt(string message)
  Console.WriteLine(message);
  return Convert.ToInt32(Console.ReadLine());
 }  
-*/  
-  
+*/   
+// Задача 27.Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+  void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}  
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}  
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);  
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
     
