b60cfac6ef334064cbd698d2c5c907b8ca593e3c
Tanya <zavoronkova1981@gmail.com>
2023-02-19 16:31:28 +0300

    added text

--- C#Lesson7/Hoomwork/Program.cs
+++ C#Lesson7/Hoomwork/Program.cs
@@ -1,4 +1,5 @@
-﻿//Задайте двумерный массив размером m×n, заполненный случайными целыми числами.  
+﻿//Задайте двумерный массив размером m×n, заполненный случайными целыми числами.   
+/* 
 int[,]CreateRandom2dArray(int rows,int columns,int minValue,int maxValua)  
 {  
     int[,]array = new int [rows,columns];  
@@ -30,7 +31,47 @@ int maxValua = Convert.ToInt32(Console.ReadLine());
   
   int[,]newArray = CreateRandom2dArray( rows, columns, minValue, maxValua);  
   Show2dArray(newArray);  
+  */ 
    
   //2/Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты
-//1.Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j. Выведите полученный массив на экран.
-//3.Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+//1.Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j. Выведите полученный массив на экра       
+//н.  
+int[,]FillArray (int rows,int columns)  
+{
+    int[,]matrix = new int [rows,columns];  
+    for(int i = 0; i < rows; i++)  
+    {
+      for(int j = 0; j < columns; j++) 
+      { 
+        matrix[i,j] = i+j;
+      }
+
+    }
+   return matrix;
+}  
+void PrintArray(int [,] matr)  
+{  
+   for(int i = 0;i < matr.GetLength(0); i++)  
+   {
+     for(int j = 0;j <matr.GetLength(1); j++)  
+     {
+      Console.Write($" {matr[i,j]}");
+
+     }
+     Console.WriteLine();
+   }
+}  
+Console.WriteLine("Input rows =");  
+int rows = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("Input columns =");  
+int columns = Convert.ToInt32(Console.ReadLine());   
+int[,]matrix = FillArray(rows,columns);  
+
+PrintArray(matrix);
+//3.Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.  
+//Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+//Задача 1.Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.(столбцы =строкам),метод преобразующий. Только пошел по нужным.  
+//Задача 2.Из двумерного массива целых чисел удалить строку и столбец, на пересечении которых расположен наим
+//еньший элемент.Найти мин элемент(индекс),сгенировать новый массив(проверка,как сделать)два метода  
+//mathprofi(произведение матриц)
