67993d92722f6496b66a0a0f41d4ef7f7e9161fb
Tanya <zavoronkova1981@gmail.com>
2023-02-20 06:55:47 +0300

    added text

--- C#Lesson8/Hoomwork/Program.cs
+++ C#Lesson8/Hoomwork/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива. 
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.  
+/* 
 void FillArrayRandom(int[,] array)
 {
     for    (int i=0;  i<array.GetLength(0); i++)
@@ -44,8 +45,104 @@ PrintArray(table);
 SortToLower(table);
 Console.WriteLine();
 PrintArray(table);
+*/ 
+//  Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.  
+/* 
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}  
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}  
+ Console.Write("Input a nmber of rows:");   
+  int rows = Convert.ToInt32(Console.ReadLine()); 
+  Console.Write("Input a nmber of columns:");  
+  int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+int[,] secondArray = new int[rows, columns];
+int[,] resultArray = new int[rows, columns];
+
+FillArrayRandom(array);
+PrintArray2D(array);
+
+Console.WriteLine();
+
+FillArrayRandom(secondArray);
+PrintArray2D(secondArray);
+
+Console.WriteLine();
+
+if (array.GetLength(0) != secondArray.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArray[i, j] += array[i, k] * secondArray[k, j];
+        }
+    }
+}
+
+PrintArray2D(resultArray);  
+*/  
+/// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу,
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.    
+/*
+ int[,,] array3D = new int[2, 2, 2]; 
+void PrintIndex(int[,,] arr)
+{
+    for    (int i=0; i < arr.GetLength(0);i++)
+    {
+        for    (int j=0; j < arr.GetLength(1);j++) 
+        {
+            Console.WriteLine();
+            for    (int k=0; k < arr.GetLength(2); k++) 
+            {
+                Console.Write($"{array3D[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}  
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+FillArray(array3D);
+PrintIndex(array3D);
+/*  
 
 
-
- 
 
