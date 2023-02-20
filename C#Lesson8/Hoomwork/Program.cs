d244e22e227e849a530710db0b9a3387b8e8b0fe
Tanya <zavoronkova1981@gmail.com>
2023-02-20 21:08:55 +0300

    added text

--- C#Lesson8/Hoomwork/Program.cs
+++ C#Lesson8/Hoomwork/Program.cs
@@ -185,4 +185,72 @@ void PrintArray(int[,] array)
         Console.WriteLine("");
     }
 }  
-*/
+*/  
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.   
+/*
+Console.Clear();
+ 
+int height = EnterInt("Введите количество строк: ");
+int width = EnterInt("Введите количество столбцов: ");
+ 
+int[,] numbers = new int[height, width];
+
+ 
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+ 
+void Fill2DArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = new Random().Next(-20, 21);
+        }
+    }
+}
+ 
+void Print2DArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)      
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)   
+        {
+            Console.Write($"{numbers[i, j],3} ");
+        }
+        Console.WriteLine();
+    }    
+}
+ 
+void SumInLines(int[,] numbers)
+        {
+            int[] sumInLines = new int[numbers.GetLength(0)];
+            Console.Write("Суммы элементов в каждой строке: ");
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    sumInLines[i] += numbers[i, j];
+                }
+                Console.Write($"{sumInLines[i]} ");
+            }
+            int minI = 0;
+            for (int i = 0; i < sumInLines.Length; i++)
+            {
+                if (sumInLines[minI] > sumInLines[i]) minI = i;
+            }
+            Console.Write($"Наименьшая сумма элементов: {sumInLines[minI]}");  
+            
+Console.Write($"Наименьшая сумма элементов: {sumInLines[minI]}, номер строки с ней: {minI + 1}");
+
+        }   
+ Fill2DArray(numbers);
+Print2DArray(numbers);
+SumInLines(numbers);  
+*/  
+
+
