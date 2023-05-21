5bf875a93445fd98885bb9d1a549d613b3d4420f
Tanya <zavoronkova1981@gmail.com>
2023-05-21 18:32:58 +0300

    added text

--- python_s5/Hoom.py
+++ python_s5/Hoom.py
@@ -1,18 +1,18 @@
-class House():
+# class House():
    
-   def __init__(self,name,surname,street,number):
+#    def __init__(self,name,surname,street,number):
       
-    self.name = name
-    self.surname = surname 
-    self.street = street
-    self.number = number
+#     self.name = name
+#     self.surname = surname 
+#     self.street = street
+#     self.number = number
                      
-   def peoplehouse(self):
-        print("Имя" + str(self.name) + "Фамилия" + str(self.surname) + "Живет на улице" + str(self.street) + "Номер" + str(self.number))
+#    def peoplehouse(self):
+#         print("Имя" + str(self.name) + "Фамилия" + str(self.surname) + "Живет на улице" + str(self.street) + "Номер" + str(self.number))
 
 
-House1 = House("Иван","Иванов","Московская", 20) 
-House2 = House("Ирина","Иванова","Серебряная", 30)
+# House1 = House("Иван","Иванов","Московская", 20) 
+# House2 = House("Ирина","Иванова","Серебряная", 30)
 
 # class PeopleHouse(House):
 #    # люди в доме
@@ -22,4 +22,86 @@ House2 = House("Ирина","Иванова","Серебряная", 30)
 # childHouse = PeopleHouse("Иванов Иван Иванович", 1)
 # House1.age_of_house(10)
 # print(childHouse.people)
-House1.peoplehouse()
+# House1.peoplehouse()
+class Address:
+    def __init__(self,
+                 area: str,
+                 city: str,
+                 street: str,
+                 number: int,
+                 kvartira: int):
+        self.area = area
+        self.city = city
+        self.street = street
+        self.number = number
+        self.kvartira = kvartira
+        self.allAddr = f"кр./обл.: {self.area}, г.: {self.city}, ул.: {self.street}, строение: {self.number}, кв.:{self.kvartira}"
+
+    def __str__(self):
+        return self.allAddr
+
+    def __repr__(self):
+        return self.allAddr
+
+
+class People:
+    def __init__(self,
+                 familia: str,
+                 name: str,
+                 otchestvo: str,
+                 birthDate: str):
+        self.familia = familia
+        self.name = name
+        self.otchestvo = otchestvo
+        self.birthDate = birthDate
+        self.allName = f"{self.familia} {self.name} {self.otchestvo} {self.birthDate} г.р."
+
+    def __str__(self):
+        return self.allName
+
+    def __repr__(self):
+        return self.allName
+
+
+class House:
+    def __init__(self, address: Address, peoples: list = []):
+        self.address = address
+        self.peoples = peoples
+        for (count, people) in enumerate(peoples, start=1):
+            self.__set_propiska_to_people(people=people, kvartira=count)
+        self.lastKvartira = count
+
+    def add_people_to_house(self, people: People):
+        self.lastKvartira += 1
+        self.peoples.append(people)
+        self.__set_propiska_to_people(people, self.lastKvartira)
+
+    def __set_propiska_to_people(self, people: People, kvartira: int):
+        tmpAddr: Address = self.address
+        tmpAddr.kvartira = kvartira
+        people.addressPropiski = tmpAddr
+
+    def showPeoples(self):
+        print(f"в доме {self.address} проживают:")
+        for people in self.peoples:
+            print(people)
+
+    def __str__(self):
+        return f"Дом по адресу {self.address}"
+
+    def __repr__(self):
+        return f"Дом по адресу {self.address}"
+
+
+addr1 = Address("Московская", "Москва", "Ленина", "12", None)
+
+people1 = People("Иванов", "Иван", "Иваныч", "09.05.1945")
+
+house1 = House(addr1, [people1])
+
+house1.add_people_to_house(People("Петров", "Петр", "Петрович", "10.05.1945"))
+
+print(addr1)
+print(people1)
+print(house1)
+house1.showPeoples()
--- python_s5/Proba.py
+++ python_s5/Proba.py
@@ -370,6 +370,110 @@
 # print(m_set)
 # s_set = sorted(n_set.intersection(m_set))
 # print(*s_set)                
+# def our_filter(func, numbers):
+#     return (el for el in numbers if func(el))
+
+# def compare_numbers(num):
+#     return num > 5
+
+# numbers = [1, 14, 6, 10, 3, 2, 5]
+
+# print( list(filter(compare_numbers, numbers)))
+# print( list(our_filter(compare_numbers, numbers)))
+# import telebot
+# import requests
+# import time
+
+# bot = telebot.TeleBot("6119292896:AAEZvqy8hW6fx42SKaWBr5_9qyI7NopYdaE") 
+
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+# 	bot.reply_to(message, "Howdy, how are you doing?")
+	
+
+# @bot.message_handler(content_types=['text'])
+# def text_message(message):
+# 	#print(message)
+# 	data = open('log.txt', mode='a', encoding='utf-8')
+# 	text = f'{message.from_user.first_name} {message.from_user.last_name}: {message.text}\n'
+# 	data.write(text)
+# 	data.close()
+	
+
+# bot.polling()
+#----
+# if message.text == 'регистрация':
+# 		data = open('registred_users.txt', mode='r', encoding='utf-8')
+# 		id_list = data.readlines()
+# 		data.close()
+# 		print(id_list)
+# 		id_list = list(el[:-1] for el in id_list)
+# 		print(id_list)
+# 		if str(message.from_user.id) not in id_list:
+# 			data = open('registred_users.txt', mode='a', encoding='utf-8')
+# 			data.write(f'{message.from_user.id}\n')
+# 			data.close()
+# 			bot.reply_to(message, 'Регистрация прошла успешно!')
+# 		else:
+# 			bot.reply_to(message, 'Вы уже зарегистрированы!')
+#####3#
+# elif message.text == 'оповещение':
+# 		data = open('registred_users.txt', mode='r', encoding='utf-8')
+# 		id_list = data.read().split('\n')
+# 		data.close()
+# 		id_list = id_list[:-1]
+# 		for id in id_list:
+# 			bot.send_message(id, 'совещание через 5 минут')
+# полностью файл
+# 
+# Здесь уже бот с кнопками
+import telebot
+from telebot import types
+import requests
+import time
+
+bot = telebot.TeleBot("6119292896:AAEZvqy8hW6fx42SKaWBr5_9qyI7NopYdaE") 
+
+markup = types.ReplyKeyboardMarkup(row_width=1)
+btn_reg = types.KeyboardButton('регистрация')
+btn_alarm = types.KeyboardButton('оповещение')
+markup.add(btn_reg, btn_alarm)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.send_message(message.from_user.id, "Привет, я бот Питоша.", reply_markup=markup)
+	
+
+@bot.message_handler(content_types=['text'])
+def text_message(message):
+	#print(message)
+	data = open('log.txt', mode='a', encoding='utf-8')
+	text = f'{message.from_user.first_name} {message.from_user.last_name}: {message.text}\n'
+	data.write(text)
+	data.close()
+	if message.text == 'регистрация':
+		data = open('registred_users.txt', mode='r', encoding='utf-8')
+		id_list = data.readlines()
+		data.close()
+		id_list = list(el[:-1] for el in id_list)
+		if str(message.from_user.id) not in id_list:
+			data = open('registred_users.txt', mode='a', encoding='utf-8')
+			data.write(f'{message.from_user.id}\n')
+			data.close()
+			bot.reply_to(message, 'Регистрация прошла успешно!')
+		else:
+			bot.reply_to(message, 'Вы уже зарегистрированы!')
+	elif message.text == 'оповещение':
+		data = open('registred_users.txt', mode='r', encoding='utf-8')
+		id_list = data.read().split('\n')
+		data.close()
+		id_list = id_list[:-1]
+		for id in id_list:
+			bot.send_message(id, 'совещание через 5 минут')
+		
+	
+bot.polling()
+
 
 
 
