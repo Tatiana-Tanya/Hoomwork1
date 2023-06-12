3ded4c68455ffcba5c9fe1407c5e23f9fb9c27e2
Tanya <zavoronkova1981@gmail.com>
2023-06-12 05:15:26 +0300

    added text

---
+++ java_s1/task2_1.java
@@ -0,0 +1,15 @@
+
+// public class task2_1 {
+//     public static void main(String[] args) {
+//         int x = 3;
+//         x = x * 17;
+//         String name = "Привет";
+//         System.out.print(" x равен " + x); 
+//     } 
+// }
+
+
+
+        
+    
+      
---
+++ java_s1/tast1_1.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+import java.util.Comparator;
+//import java.util.Random;
+
+public class tast1_1 {
+    public static void main(String[] args) {
+         
+                    
+       ArrayList<String> list = new ArrayList<>();
+       list.add ( "Земля");
+       list.add ( "Марс");
+       list.add ( "Юпитер");
+       list.add ("Юпитер");
+       list.add ( "Венера");
+       list.add ( "Плутон");
+       list.add ( "Сатурн");
+       list.add ( "Земля");
+       list.add ( "Земля");
+       System.out.println(list);
+       list.sort(Comparator.naturalOrder());
+
+       System.out.println(list);
+    }
+}   
+
+
